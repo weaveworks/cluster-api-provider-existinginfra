54bb8d201e383b1a88a2265b28b0cdd9f28da124
Dennis Marttinen <dennis@weave.works>
2020-08-20 16:53:27 +0300

    Add CAPI v1alpha3 types to the scheme in main.go
    Signed-off-by: Dennis Marttinen <dennis@weave.works>

--- main.go
+++ main.go
@@ -21,9 +21,11 @@ import (
 	"os"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	clusterv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -44,10 +46,11 @@ var (
 )
 
 func init() {
-	_ = clientgoscheme.AddToScheme(scheme)
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(clusterv1alpha3.AddToScheme(scheme))
 
-	_ = clusterweaveworksv1alpha3.AddToScheme(scheme)
-	_ = baremetalproviderspecv1alpha1.AddToScheme(scheme)
+	utilruntime.Must(clusterweaveworksv1alpha3.AddToScheme(scheme))
+	utilruntime.Must(baremetalproviderspecv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
