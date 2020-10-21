3aff27919c8435a0ca6192f8f18d2676e7c02cab
Jerry Jackson <jerry@weave.works>
2020-10-21 10:41:27 -0600

    try using a more current cert-manager

--- test/integration/test/workload_cluster_test.go
+++ test/integration/test/workload_cluster_test.go
@@ -122,7 +122,7 @@ func TestWorkloadClusterCreation(t *testing.T) {
 func installCertManager(c *context) {
 	log.Info("Installing cert manager...")
 	// kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.11.1/cert-manager.yaml
-	c.runAndCheckError("kubectl", "apply", "--validate=false", "-f", "https://github.com/jetstack/cert-manager/releases/download/v0.11.1/cert-manager.yaml")
+	c.runAndCheckError("kubectl", "apply", "--validate=false", "-f", "https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml")
 }
 
 func setupNetworking(c *context) {
