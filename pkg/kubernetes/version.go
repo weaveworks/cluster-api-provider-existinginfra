c09c3a15882d8ba49226a3bc05ed2087534915d7
Dinos Kousidis <dinos@weave.works>
2020-10-28 18:23:53 +0100

    Increase accepted versions to 1.19

--- pkg/kubernetes/version.go
+++ pkg/kubernetes/version.go
@@ -3,5 +3,5 @@ package kubernetes
 // DefaultVersion is the default Kubernetes version used by WKS.
 const (
 	DefaultVersion       = "1.16.11"
-	DefaultVersionsRange = ">=1.14.1 <=1.17.x"
+	DefaultVersionsRange = ">=1.14.1 <=1.19.x"
 )
