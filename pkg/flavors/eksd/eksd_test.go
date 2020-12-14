ae2359302a9a79ce19f7c473809424096cd1e5a2
Mark Emeis <mark.emeis@weave.works>
2020-12-14 11:20:36 -0700

    Image and tag parsing from eksd manifest

---
+++ pkg/flavors/eksd/eksd_test.go
@@ -0,0 +1,57 @@
+package eksd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestImageAndImageTag(t *testing.T) {
+	// TODO replace with static text so we aren't relying on this URL
+	e, err := New("https://distro.eks.amazonaws.com/kubernetes-1-18/kubernetes-1-18-eks-1.yaml")
+	assert.NoError(t, err)
+	tests := []struct {
+		name    string
+		expRepo string
+		expTag  string
+		expErr  bool
+	}{
+		{"etcd", "public.ecr.aws/eks-distro/etcd-io", "v3.4.14-eks-1-18-1", false},
+		{"eTCd", "public.ecr.aws/eks-distro/etcd-io", "v3.4.14-eks-1-18-1", false},
+		{"goober", "", "", true},
+	}
+	for _, test := range tests {
+		repo, tag, err := e.ImageInfo(test.name)
+		if test.expErr {
+			assert.Error(t, err)
+		} else {
+			assert.Equal(t, test.expRepo, repo)
+			assert.Equal(t, test.expTag, tag)
+			assert.NoError(t, err)
+		}
+	}
+}
+
+func TestKubeBin(t *testing.T) {
+	// TODO replace with static text so we aren't relying on this URL
+	e, err := New("https://distro.eks.amazonaws.com/kubernetes-1-18/kubernetes-1-18-eks-1.yaml")
+	assert.NoError(t, err)
+	tests := []struct {
+		name   string
+		expErr bool
+	}{
+		{"kubelet", false},
+		{"kubectl", false},
+		{"goober", true},
+	}
+	for _, test := range tests {
+		url, sha, err := e.KubeBinURL(test.name)
+		if test.expErr {
+			assert.Error(t, err)
+		} else {
+			assert.NotEqual(t, "", url)
+			assert.NotEqual(t, "", sha)
+			assert.NoError(t, err)
+		}
+	}
+}
