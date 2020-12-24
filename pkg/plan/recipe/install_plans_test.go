5ade1ce0106af628339ce95260e9a4e31fea135e
Mark Emeis <mark.emeis@weave.works>
2020-12-24 08:40:14 -0700

    Verify sha of eks-d binaries on download (#55)

--- pkg/plan/recipe/install_plans_test.go
+++ pkg/plan/recipe/install_plans_test.go
@@ -1,6 +1,7 @@
 package recipe
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -49,4 +50,18 @@ func TestBinInstallerFlavor(t *testing.T) {
 	assert.Contains(t, res.Script, "curl -o /usr/bin/kubectl https")
 	assert.Contains(t, res.Script, "chmod 755 /usr/bin/kubectl")
 
+}
+
+func TestSHACheck(t *testing.T) {
+	cf, err := eksd.New("https://distro.eks.amazonaws.com/kubernetes-1-18/kubernetes-1-18-eks-1.yaml")
+	assert.NoError(t, err)
+
+	f, err := BinInstaller(resource.PkgTypeDeb, cf)
+	assert.NoError(t, err)
+	assert.NotNil(t, f)
+	res, ok := f("kubelet", "v2.3").(*resource.Run)
+	_, sha256, err := cf.KubeBinURL("kubelet")
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Contains(t, res.Script, fmt.Sprintf("openssl dgst -sha256 /usr/bin/kubelet | grep \"%s\" > /dev/null", sha256))
 }
